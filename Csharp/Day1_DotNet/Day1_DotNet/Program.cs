131d5bff803b7818e011aac05d25b0684541a033
Siddartha Nainala <siddarthan@infinite.com>
2024-06-13 14:01:08 +0530

    DotNet_day1

---
+++ Csharp/Day1_DotNet/Day1_DotNet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_DotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name  ");
+           string name =  Console.ReadLine();
+            Console.WriteLine("Shownig your name using concatination "+name);
+            Console.WriteLine("showing your name using placeholder {0}", name);
+            Console.Read();
+
+        }
+    }
+}
