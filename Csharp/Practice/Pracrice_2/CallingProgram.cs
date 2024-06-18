0ec2117d85686465613a39d8e3a4f0f3cc9fbf1f
siddarthan <siddarthan@DESKTOP-VC7B2E1>
2024-06-18 23:48:06 +0530

    prac

---
+++ Csharp/Practice/Pracrice_2/CallingProgram.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Practice;
+namespace Pracrice_2
+{
+    public class CallingProgram
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine(p.print());
+            Console.WriteLine("hello iam referencing another project ");
+            
+            Console.Read();
+        }
+    }
+}
---
+++ Csharp/Practice/Practice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Practice
+{
+    
+    public class Program
+    {
+       
+        public  string print()
+        {
+            return "success";
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+        }
+    }
+}
